875d93e5f1203e6cc0aa037e27f6b7e9a3472667
caijoseph <tianyucai0816c@gmail.com>
2023-10-31 12:26:25 +0000

    Users updated

--- _InitialiseDatabase.php
+++ _InitialiseDatabase.php
@@ -34,7 +34,12 @@ if (!$conn) {
 $sql = "CREATE TABLE Users (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 username VARCHAR(255) NOT NULL,
-password VARCHAR(255) NOT NULL)";
+password VARCHAR(255) NOT NULL,
+email VARCHAR(255) NOT NULL,
+first_name VARCHAR(255) NOT NULL,
+last_name VARCHAR(255) NOT NULL,
+address VARCHAR(255),
+create_date date)";
 
 if (mysqli_query($conn, $sql)) {
     echo "Table created successfully";

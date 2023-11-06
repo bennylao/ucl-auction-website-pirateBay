75593d79bba866de38216a55f8712dd4913e1886
victorchan <victor2263@gmail.com>
2023-11-06 15:53:23 +0000

    Updated wishlist.

--- wishlist.php
+++ wishlist.php
@@ -1,9 +1,14 @@
-<?php include_once("header.php")?>
-<?php require("utilities.php")?>
+<?php
+$title = "Auction Wishlist";
+include_once ("header.php")?>
+<?php require ("utilities.php")?>
+<?php require_once ("config_database.php")?>
+
+
 
 <div class="container">
 
-<h2 class="my-3">Wishlist</h2>
+<h2 class="my-3">My wishlist</h2>
 
 <div id="searchSpecs">
 <!-- When this form is submitted, this PHP page is what processes it.
@@ -107,26 +112,33 @@
      retrieved from the query -->
 
 <?php
-  // Demonstration of what listings will look like using dummy data.
-  $item_id = "87021";
-  $title = "Dummy title";
-  $description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget rutrum ipsum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus feugiat, ipsum vel egestas elementum, sem mi vestibulum eros, et facilisis dui nisi eget metus. In non elit felis. Ut lacus sem, pulvinar ultricies pretium sed, viverra ac sapien. Vivamus condimentum aliquam rutrum. Phasellus iaculis faucibus pellentesque. Sed sem urna, maximus vitae cursus id, malesuada nec lectus. Vestibulum scelerisque vulputate elit ut laoreet. Praesent vitae orci sed metus varius posuere sagittis non mi.";
-  $current_price = 30;
-  $num_bids = 1;
-  $end_date = new DateTime('2020-09-16T11:00:00');
-  
-  // This uses a function defined in utilities.php
-  print_listing_li($item_id, $title, $description, $current_price, $num_bids, $end_date);
-  
-  $item_id = "516";
-  $title = "Different title";
-  $description = "Very short description.";
-  $current_price = 13.50;
-  $num_bids = 3;
-  $end_date = new DateTime('2020-11-02T00:00:00');
-  
-  print_listing_li($item_id, $title, $description, $current_price, $num_bids, $end_date);
+
+// Create database connection
+$connection = connect_to_database();
+
+// SQL to fetch data
+    $query = "SELECT id, name, category, description, current_price, num_bids, end_datetime FROM Items";
+    $result = mysqli_query($connection,$query);
+
+if ($result->num_rows > 0) {
+    // Output data of each row
+    while($row = $result->fetch_assoc()) {
+        $item_id = $row["id"];
+        $title = $row["name"];
+        $category = $row["category"];
+        $description = $row["description"];
+        $current_price = $row["current_price"];
+        $num_bids = $row["num_bids"];
+        $end_datetime = new DateTime($row["end_datetime"]);
+        // This uses a function defined in utilities.php
+        print_listing_li($item_id, $title, $category, $description, $current_price, $num_bids, $end_datetime);
+    }
+} else {
+    echo "No results found.";
+}
+mysqli_close($connection);
 ?>
+
 
 </ul>
 

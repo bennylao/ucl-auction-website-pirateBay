8c4d903d780de3c6a5b00ca20f926159d9775df9
victorchan <victor2263@gmail.com>
2023-11-11 18:21:00 +0000

    new dummy data for item description

--- listing.php
+++ listing.php
@@ -98,7 +98,7 @@ mysqli_close($connection);
           <?php else: ?>
         Auction ends <?php echo(date_format($end_time, 'j M H:i') . $time_remaining) ?></p>
       <p class="lead">Current bid: £<?php echo(number_format($current_price, 2)) ?></p>
-        <p class="lead">Total bids: £<?php echo(number_format($num_bids)) ?></p>
+        <p class="lead">Total bids: <?php echo(number_format($num_bids)) ?></p>
 
       <!-- Bidding form -->
       <form method="POST" action="place_bid.php">
